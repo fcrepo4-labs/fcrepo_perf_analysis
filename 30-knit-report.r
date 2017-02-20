7692e9320d343b7284dd339a272adfa4eb814f93
Colin Gross <grosscol@gmail.com>
2017-02-20 09:37:51 -0500

    Use relative links for reports.   Avoid rmarkdown option with output directory.   Move resulting files around after generation.

--- 30-knit-report.r
+++ 30-knit-report.r
@@ -1,6 +1,7 @@
 require('knitr')
 require('rmarkdown')
 require('ggplot2')
+library('tools')
 
 # Get command line args entered by the user
 options <- commandArgs(TRUE)
@@ -22,4 +23,21 @@ if(length(options) < 2){
 dir.create(artifacts_dir, showWarnings=FALSE, mode='0664')
 
 rmarkdown::render("31-github-report.rmd", params = list(input_dir = artifacts_dir, output_name = output_name),
-                  output_file=output_name, output_dir=artifacts_dir)
+                  output_file=output_name)
+
+# Get the basename of the report output  
+report_name <- file_path_sans_ext( basename(output_name) )
+
+# Move resulting report into artifacts directory
+# Move primary file
+file.rename(output_name, file.path(artifacts_dir, output_name))
+# if extant, move the html preview as well
+html_preview <- paste0(report_name,'.html')
+if( file.exists(html_preview) ){
+  file.rename(html_preview, file.path(artifacts_dir, html_preview))
+}
+
+# Move resulting imgages directory into artifacts directory
+imgs_dirname <- paste0(report_name,"_files")
+file.rename(imgs_dirname, file.path(artifacts_dir, imgs_dirname))
+
